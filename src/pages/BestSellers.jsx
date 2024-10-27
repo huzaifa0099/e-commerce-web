44df39206f00cf982205faf5ca92d0d6e06c1b3c
Huzaifa Ali <huzaifaali006@gmail.com>
2024-10-27 01:37:26 -0700

    useEffect

--- src/pages/BestSellers.jsx
+++ src/pages/BestSellers.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
